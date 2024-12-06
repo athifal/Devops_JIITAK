a3879b3d5151e9094b55530b2ff31eb713cc1a23
unknown <athif105@gmail.com>
2024-12-06 14:07:02 +0530

    trouble shoot

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home():
         </head>
         <body style="display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #f0f8ff;">
             <h1 style="color: #0000FF; font-family: Arial, sans-serif; text-align: center;">
-                Hello, JITTAK! Welcome to the DevOps Assignment!
+                Hello, JITTAK!4 Welcome to the DevOps Assignment!
             </h1>
         </body>
     </html>
