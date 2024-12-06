2830c5be79ab1442228a33d85f92598d55305d05
unknown <athif105@gmail.com>
2024-12-06 11:33:28 +0530

    pushin DockerFile and code

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+# Use the official Python base image
+FROM python:3.9-alpine
+WORKDIR /app
+COPY . /app
+RUN pip install --no-cache-dir -r requirements.txt
+EXPOSE 5000
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <html>
+        <head>
+            <title>DevOps Assignment</title>
+        </head>
+        <body style="display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #f0f8ff;">
+            <h1 style="color: #0000FF; font-family: Arial, sans-serif; text-align: center;">
+                Hello, World! Welcome to the DevOps Assignment!
+            </h1>
+        </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
